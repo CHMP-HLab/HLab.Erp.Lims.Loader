7f023ffc471221e9a33307470b76d23a8752e76e
mgth <mathieu@mgth.fr>
2021-07-22 11:31:59 +0200

    Product category support

--- HLab.Erp.Lims.Loader/App.xaml.cs
+++ HLab.Erp.Lims.Loader/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Windows;
+using HLab.Bugs.Wpf;
 using HLab.Core;
 using HLab.Core.Annotations;
 using HLab.Core.DebugTools;
