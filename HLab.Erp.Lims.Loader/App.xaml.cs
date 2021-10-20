1905d6e631cfd722fd6cb2431497df7bfa54e92b
mgth <mathieu@mgth.fr>
2021-10-20 10:18:44 +0200

    v 2.2.8

--- HLab.Erp.Lims.Loader/App.xaml.cs
+++ HLab.Erp.Lims.Loader/App.xaml.cs
@@ -55,153 +55,149 @@ namespace HLab.Erp.Lims.Analysis.Loader
         protected override void OnStartup(StartupEventArgs e)
         {
 #if !DEBUG
-            
             try
             {
 #endif
+                base.OnStartup(e);
 
-            base.OnStartup(e);
+                var parser = new AssemblyParser();
+                Locator.Configure();
 
-            var parser = new AssemblyParser();
-            Locator.Configure();
+                SingletonLocator<IOptionsService>.Set<OptionsServices>();
 
-            SingletonLocator<IOptionsService>.Set<OptionsServices>();
+                SingletonLocator<IEventHandlerService>.Set<EventHandlerServiceWpf>();
+                SingletonLocator<IAclService>.Set<AclService>();
+                SingletonLocator<IAclHelper>.Set<AclHelperWpf>();
+                SingletonLocator<IDebugLogger>.Set<DebugLogger>();
+                SingletonLocator<IDataService>.Set<DataService>();
+                SingletonLocator<IMessageBus>.Set<MessageBus>();
+                SingletonLocator<IDragDropService>.Set<DragDropServiceWpf>();
+                SingletonLocator<IDialogService>.Set<DialogService>();
+                SingletonLocator<ICurrencyService>.Set<CurrencyService>();
 
-            SingletonLocator<IEventHandlerService>.Set<EventHandlerServiceWpf>();
-            SingletonLocator<IAclService>.Set<AclService>();
-            SingletonLocator<IAclHelper>.Set<AclHelperWpf>();
-            SingletonLocator<IDebugLogger>.Set<DebugLogger>();
-            SingletonLocator<IDataService>.Set<DataService>();
-            SingletonLocator<IMessageBus>.Set<MessageBus>();
-            SingletonLocator<IDragDropService>.Set<DragDropServiceWpf>();
-            SingletonLocator<IDialogService>.Set<DialogService>();
-            SingletonLocator<ICurrencyService>.Set<CurrencyService>();
-            
-            SingletonLocator<IUnitService>.Set<UnitService>();
+                SingletonLocator<IUnitService>.Set<UnitService>();
 
-            SingletonLocator<IGraphService>.Set<GraphService>();
-            SingletonLocator<IBrowserService>.Set<BrowserViewModel>();
-            SingletonLocator<IIconService>.Set<IconService>();
-            SingletonLocator<ILocalizationService>.Set<LocalizationService>();
-            SingletonLocator<IMvvmService>.Set<MvvmServiceWpf>();
-            SingletonLocator<IErpServices>.Set<ErpServices>();
-            SingletonLocator<IApplicationInfoService>.Set<ApplicationInfoService>();
-            SingletonLocator<IDocumentService>.Set<DocumentServiceWpf>();
-            SingletonLocator<IMenuService>.Set<MenuService>();
+                SingletonLocator<IGraphService>.Set<GraphService>();
+                SingletonLocator<IBrowserService>.Set<BrowserViewModel>();
+                SingletonLocator<IIconService>.Set<IconService>();
+                SingletonLocator<ILocalizationService>.Set<LocalizationService>();
+                SingletonLocator<IMvvmService>.Set<MvvmServiceWpf>();
+                SingletonLocator<IErpServices>.Set<ErpServices>();
+                SingletonLocator<IApplicationInfoService>.Set<ApplicationInfoService>();
+                SingletonLocator<IDocumentService>.Set<DocumentServiceWpf>();
+                SingletonLocator<IMenuService>.Set<MenuService>();
 
-            SingletonLocator<LocalizeFromDb>.Set<LocalizeFromDb>();
+                SingletonLocator<LocalizeFromDb>.Set<LocalizeFromDb>();
 
-            Locator<ILoginViewModel>.Set<LoginViewModel>();
-            Locator<IAuditTrailProvider>.Set<AuditTrailMotivationViewModel>();
-            Locator<ISelectedMessage>.Set<SelectedMessage>();
+                Locator<ILoginViewModel>.Set<LoginViewModel>();
+                Locator<IAuditTrailProvider>.Set<AuditTrailMotivationViewModel>();
+                Locator<ISelectedMessage>.Set<SelectedMessage>();
 
-            SingletonLocator<MainWpfViewModel>.Set<MainWpfViewModel>();
+                SingletonLocator<MainWpfViewModel>.Set<MainWpfViewModel>();
 
+                Locator.SetOpenGenericFactory(typeof(IEntityListHelper<>), typeof(EntityListHelper<>));
+                Locator.SetOpenGenericFactory(typeof(IColumnsProvider<>), typeof(ColumnsProvider<>));
+                Locator.SetOpenGenericFactory(typeof(IObservableQuery<>), typeof(ObservableQuery<>));
+                //Locator.SetOpenGenericFactory(typeof(IEntityListViewModel<>),typeof(EntityListViewModel<>));
+                Locator.SetOpenGenericFactory(typeof(IDataLocker<>), typeof(DataLocker<>));
 
-
-            Locator.SetOpenGenericFactory(typeof(IEntityListHelper<>),typeof(EntityListHelper<>));
-            Locator.SetOpenGenericFactory(typeof(IColumnsProvider<>),typeof(ColumnsProvider<>));
-            Locator.SetOpenGenericFactory(typeof(IObservableQuery<>),typeof(ObservableQuery<>));
-            //Locator.SetOpenGenericFactory(typeof(IEntityListViewModel<>),typeof(EntityListViewModel<>));
-            Locator.SetOpenGenericFactory(typeof(IDataLocker<>),typeof(DataLocker<>));
-
-            NotifyHelper.EventHandlerService = Locate<IEventHandlerService>();
+                NotifyHelper.EventHandlerService = Locate<IEventHandlerService>();
 
 
-            _ = SampleWorkflow.Production;
-            _ = SampleTestWorkflow.ValidatedResults;
-            _ = SampleTestResultWorkflow.Checked;
+                _ = SampleWorkflow.Production;
+                _ = SampleTestWorkflow.ValidatedResults;
+                _ = SampleTestResultWorkflow.Checked;
 
 
-            //Locator.SetFactory(typeof(IDataLocker<>),);
+                //Locator.SetFactory(typeof(IDataLocker<>),);
 
-/*            
-
-            
-            .Export(typeof(DataLocker<>)).As(typeof(IDataLocker<>))
-
-              .Export(typeof(ColumnsProvider<>)).As(typeof(IColumnsProvider<>))
-             .Export(typeof(ListableEntityListViewModel<>)).As(typeof(IListableEntityListViewModel<>))
-             .Export(typeof(ColumnConfigurator<,,>)).As(typeof(IColumnConfigurator<,,>))
-             .Export(typeof(ColumnHelper<>)).As(typeof(IColumn<>.IHelper))
-             .Export(typeof(ObservableQuery<>)).As(typeof(ObservableQuery<>))
-             .Export(typeof(EntityListHelper<>)).As(typeof(IEntityListHelper<>))
-
-             .Export<MvvmContext>().As<IMvvmContext>()
-             .Export<SelectedMessage>().As<ISelectedMessage>()
-             .Export<DetailsPanelViewModel>().As<DetailsPanelViewModel>()
-             
-             );
-;
-                
-*/
-            //boot.Scope.ExportInitialize<BootLoaderErpWpf>(b => b.SetMainViewMode(typeof(ViewModeKiosk)));
+                /*            
 
 
-            //var a0 = boot.LoadDll("HLab.Erp.Core.Wpf");
-            var a01 = parser.LoadDll("HLab.Options.Wpf");
-            var a3 = parser.LoadDll("HLab.Notify.Wpf");
-            var a2 = parser.LoadDll("HLab.Erp.Base.Wpf");
-            //  var b0 = boot.LoadDll("HLab.Mvvm");
-            //  var c0 = boot.LoadDll("HLab.Mvvm.Wpf");
-            //  var d0 = boot.LoadDll("HLab.Erp.Data");
-            var d1 = parser.LoadDll("HLab.Erp.Data.Wpf");
-            var e0 = parser.LoadDll("HLab.Erp.Acl.Wpf");
-            var a1 = parser.LoadDll("HLab.Erp.Workflows.Wpf");
-            var g0 = parser.LoadDll("HLab.Erp.Lims.Analysis.Data");
-            var g2 = parser.LoadDll("HLab.Erp.Lims.Analysis.Module");
-            //var g1 = boot.LoadDll("HLab.Erp.Lims.Monographs.Module");
+                            .Export(typeof(DataLocker<>)).As(typeof(IDataLocker<>))
 
-            parser.LoadModules();
+                              .Export(typeof(ColumnsProvider<>)).As(typeof(IColumnsProvider<>))
+                             .Export(typeof(ListableEntityListViewModel<>)).As(typeof(IListableEntityListViewModel<>))
+                             .Export(typeof(ColumnConfigurator<,,>)).As(typeof(IColumnConfigurator<,,>))
+                             .Export(typeof(ColumnHelper<>)).As(typeof(IColumn<>.IHelper))
+                             .Export(typeof(ObservableQuery<>)).As(typeof(ObservableQuery<>))
+                             .Export(typeof(EntityListHelper<>)).As(typeof(IEntityListHelper<>))
 
-            parser.Add<IView>(EnumerableLocator<IView>.AddAutoFactory);
-            parser.Add<IViewModel>(EnumerableLocator<IViewModel>.AddAutoFactory);
+                             .Export<MvvmContext>().As<IMvvmContext>()
+                             .Export<SelectedMessage>().As<ISelectedMessage>()
+                             .Export<DetailsPanelViewModel>().As<DetailsPanelViewModel>()
 
-            parser.Add<IBootloader>(EnumerableLocator<IBootloader>.AddAutoFactory);
-            parser.Add<IToolGraphBlock>(EnumerableLocator<IToolGraphBlock>.AddAutoFactory);
-            parser.Add<IEntityListViewModel>(listType =>
-            {
-                if(listType.IsGenericType) return;
+                             );
+                ;
 
-                foreach(var interfaceType in listType.GetInterfaces().Where(i => i.IsGenericType))
+                */
+                //boot.Scope.ExportInitialize<BootLoaderErpWpf>(b => b.SetMainViewMode(typeof(ViewModeKiosk)));
+
+
+                //var a0 = boot.LoadDll("HLab.Erp.Core.Wpf");
+                var a01 = parser.LoadDll("HLab.Options.Wpf");
+                var a3 = parser.LoadDll("HLab.Notify.Wpf");
+                var a2 = parser.LoadDll("HLab.Erp.Base.Wpf");
+                //  var b0 = boot.LoadDll("HLab.Mvvm");
+                //  var c0 = boot.LoadDll("HLab.Mvvm.Wpf");
+                //  var d0 = boot.LoadDll("HLab.Erp.Data");
+                var d1 = parser.LoadDll("HLab.Erp.Data.Wpf");
+                var e0 = parser.LoadDll("HLab.Erp.Acl.Wpf");
+                var a1 = parser.LoadDll("HLab.Erp.Workflows.Wpf");
+                var g0 = parser.LoadDll("HLab.Erp.Lims.Analysis.Data");
+                var g2 = parser.LoadDll("HLab.Erp.Lims.Analysis.Module");
+                //var g1 = boot.LoadDll("HLab.Erp.Lims.Monographs.Module");
+
+                parser.LoadModules();
+
+                parser.Add<IView>(EnumerableLocator<IView>.AddAutoFactory);
+                parser.Add<IViewModel>(EnumerableLocator<IViewModel>.AddAutoFactory);
+
+                parser.Add<IBootloader>(EnumerableLocator<IBootloader>.AddAutoFactory);
+                parser.Add<IToolGraphBlock>(EnumerableLocator<IToolGraphBlock>.AddAutoFactory);
+                parser.Add<IEntityListViewModel>(listType =>
                 {
-                    if(interfaceType.GetGenericTypeDefinition()==typeof(IEntityListViewModel<>))
+                    if (listType.IsGenericType) return;
+
+                    foreach (var interfaceType in listType.GetInterfaces().Where(i => i.IsGenericType))
                     {
-                        var entityType = interfaceType.GetGenericArguments()[0];
-                        Locator.Set(interfaceType,listType);
+                        if (interfaceType.GetGenericTypeDefinition() == typeof(IEntityListViewModel<>))
+                        {
+                            var entityType = interfaceType.GetGenericArguments()[0];
+                            Locator.Set(interfaceType, listType);
+                        }
                     }
-                }
-            });
+                });
 
-            parser.Parse();
+                parser.Parse();
 
-            Locator.InitSingletons();
+                Locator.InitSingletons();
 
-            var options = Locate<IOptionsService>();
+                var options = Locate<IOptionsService>();
                 options.OptionsPath = "HLab.Erp";
                 options.AddProvider(new OptionsProviderRegistry());
 
-            //STATIC IMPORTS//
-            NotifyHelper.EventHandlerService = Locate<IEventHandlerService>();
-            WorkflowAnalysisExtension.Acl = Locate<IAclService>();
+                //STATIC IMPORTS//
+                NotifyHelper.EventHandlerService = Locate<IEventHandlerService>();
+                WorkflowAnalysisExtension.Acl = Locate<IAclService>();
 
-            var mvvm = Locate<IMvvmService>();
+                var mvvm = Locate<IMvvmService>();
 
-            mvvm.Register(typeof(Customer), typeof(CustomerViewModel), typeof(IViewClassDocument), typeof(ViewModeDefault));
-            mvvm.Register(typeof(Manufacturer), typeof(ManufacturerViewModel), typeof(IViewClassDocument), typeof(ViewModeDefault));
-            mvvm.Register();
+                mvvm.Register(typeof(Customer), typeof(CustomerViewModel), typeof(IViewClassDocument), typeof(ViewModeDefault));
+                mvvm.Register(typeof(Manufacturer), typeof(ManufacturerViewModel), typeof(IViewClassDocument), typeof(ViewModeDefault));
+                mvvm.Register();
 
-            var doc = Locate<IDocumentService>();
-            doc.MainViewModel = Locate<MainWpfViewModel>();
+                var doc = Locate<IDocumentService>();
+                doc.MainViewModel = Locate<MainWpfViewModel>();
 
-            var boot = new Bootstrapper(()=> Locate<IEnumerable<IBootloader>>());
+                var boot = new Bootstrapper(() => Locate<IEnumerable<IBootloader>>());
 
-            boot.Boot();
+                boot.Boot();
 #if !DEBUG
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                var view = new ExceptionView {Exception = ex};
+                var view = new ExceptionView { Exception = ex };
                 view.ShowDialog();
                 throw;
                 ExceptionDispatchInfo.Capture(ex).Throw();
@@ -209,7 +205,7 @@ namespace HLab.Erp.Lims.Analysis.Loader
 #endif        
         }
 
-       
+
 
     }
 }
